a62c044479b6408d5f40bbd48b86b068fab54869
igorForPeace <87523413+igorForPeace@users.noreply.github.com>
2022-07-21 16:47:14 +0300

    Add files via upload

---
+++ WinFormsDZ5/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsDZ5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Consolas", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(38, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.IndianRed;
+            this.label2.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(38, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WinFormsDZ5/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDZ5
+{
+    public partial class Form1 : Form
+    {
+        private DateTime newYear = new DateTime(2023, 1, 1, 0, 0, 0);
+        private DateTime myBirthday = new DateTime(2023,3,29,2,0,0);
+        private DateTime today = new DateTime();
+        private TimeSpan beforeNewYear = new TimeSpan();
+        private TimeSpan beforeMyBirthday = new TimeSpan();
+        
+        public Form1()
+        {
+            InitializeComponent();          
+            today = DateTime.Now;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.today = DateTime.Now;
+            this.beforeNewYear = this.newYear.Subtract(this.today);
+            this.label1.Text = "До нового года осталось: "+ this.beforeNewYear.ToString().Remove(12);
+            this.beforeMyBirthday = this.myBirthday.Subtract(this.today);
+            this.label2.Text = "До моего дня рождения осталось: "+ this.beforeMyBirthday.ToString().Remove(12);
+        }
+    }
+}
